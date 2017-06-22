23725ced9acb369d98c5cb4c1a3230635e1ff42e
CodeKittey <wing121@web.de>
2017-06-22 23:10:33 +0200

    added exercises express

---
+++ ExpressExercise/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.end('Hello World!');
+});
+
+app.listen(13337);
---
+++ web/expressWeb/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/home', function(req, res) {
+  res.end('Hello World!');
+});
+
+app.listen(13337);
